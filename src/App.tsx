774cf0d3b625974d1068eb523716c62572c8fc1b
KKB\model <asdf@gmail.com>
2025-01-14 22:22:18 +0900

    design : 테이블 디자인 설정정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import InfinityTable from "./components/molcule/table/InfinityTable.tsx";
+import styles from "./App.module.scss";
 
 function App() {
-  return <div className="App">뭐죠</div>;
+  const tableDataArr = [
+    { id: 1, count: 1, data: 1 },
+    { id: 2, count: 2, data: 2 },
+  ];
+  return (
+    <div className="App">
+      <div className={styles["global-wrapper"]}>
+        <h2>Infinity Table</h2>
+        <InfinityTable tableDataArr={tableDataArr} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/molcule/table/InfinityTable.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./InfinityTable.module.scss";
+
+const InfinityTable = ({ tableDataArr }) => {
+  return (
+    <div className={styles["infinity-table-wrapper"]}>
+      <div className={styles["grid-table"]}>
+        <div className={styles["grid-header"]}>
+          <div className={styles["grid-cell"]}>
+            <input type="checkbox" />
+          </div>
+          <div className={styles["grid-cell"]}>조회</div>
+          <div className={styles["grid-cell"]}>구분</div>
+          <div className={styles["grid-cell"]}>이름</div>
+          <div className={styles["grid-cell"]}>담당 클래스</div>
+          <div className={styles["grid-cell"]}>아이디</div>
+        </div>
+        {tableDataArr.map((it, idx) => (
+          <div className={styles["grid-row"]}>
+            <div className={styles["grid-cell"]}>
+              <input type="checkbox" />
+            </div>
+            <div className={styles["grid-cell"]}>▶</div>
+            <div className={styles["grid-cell"]}>선생님</div>
+            <div className={styles["grid-cell"]}>박규범_선생님</div>
+            <div className={styles["grid-cell"]}></div>
+            <div className={styles["grid-cell"]}>test1</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default InfinityTable;
